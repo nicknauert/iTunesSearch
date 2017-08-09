11237f3dc23d79f7b29390418958ab56f60b8be3
Nick Nauert <nicknauert@gmail.com>
2017-08-09 14:33:05 -0400

    search implemented

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let searchBar = document.querySelector(".textInput")
-let button = document.querySelector(".searchBtn")
+let searchBar = document.querySelector(".textInput");
+let button = document.querySelector(".searchBtn");
+let resultsCont = document.querySelector(".results");
 
 searchBar.addEventListener('keydown', function(event){
   if (event.which == 13 || event.keyCode == 13) {
@@ -12,23 +13,40 @@ searchBar.addEventListener('keydown', function(event){
 button.addEventListener('click', searchMusic);
 
 
+function clearResults(){
+  resultsCont.innerHTML = "";
+}
+
+
 //The goods are below
 //
 //${artistName} ${artworkUrl100} ${trackName} ${previewUrl} ${trackViewUrl}
 // ${artistViewUrl}
-//ISO 3166-2:US
+// US
 
 
 function searchMusic(){
   let input = encodeURI(searchBar.value);
   let url = "https://itunes.apple.com/search?term=" + input + "&country=US";
 
+  clearResults();
 
   fetch(url)
     .then( function (data) {
       return data.json();
     })
     .then( function (data) {
-      console.log(data);
+
+      for(i=0;i<20;i++){
+        let item = data.results;
+        let tmpl =
+          `<div class="resultItem">
+            <img src="${item[i].artworkUrl100}" alt="Album Cover">
+            <a class="songTitle" href="${item[i].trackViewUrl}"><p>${item[i].trackName}</p></a>
+            <a class="artistName" href="${item[i].artistViewUrl}"><p>${item[i].artistName}</p></a>
+          </div>`;
+
+          resultsCont.innerHTML += tmpl;
+      }
     })
 }
