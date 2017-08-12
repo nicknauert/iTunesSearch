f8921ea919f8baac776b8e8bbc790c2b1cd0d81b
Nick Nauert <nicknauert@gmail.com>
2017-08-12 18:55:38 -0400

    custom player mostly working

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-let searchBar = document.querySelector(".textInput");
-let button = document.querySelector(".searchBtn");
-let resultsCont = document.querySelector(".results");
-let player = document.querySelector('.music-player');
+const searchBar = document.querySelector(".textInput");
+const button = document.querySelector(".searchBtn");
+const playBtn = document.querySelector(".playBtn");
+const resultsCont = document.querySelector(".results");
+const player = document.querySelector('.music-player');
+const progressBar = document.querySelector(".progressBar");
+const volumeSlider = document.querySelector(".volume");
+let audioState = false;
+
 
 searchBar.addEventListener('keydown', function(event){
   if (event.which == 13 || event.keyCode == 13) {
@@ -13,18 +18,55 @@ searchBar.addEventListener('keydown', function(event){
 
 button.addEventListener('click', searchMusic);
 
-//The goods are below
-//
-//${artistName} ${artworkUrl100} ${trackName} ${previewUrl} ${trackViewUrl}
-// ${artistViewUrl}
-// US
+
+
+// =========== Music Player Functions ================
+
+
+function createSong(url){ /*creates the audio object*/
+  if(audioState === true)
+  {
+    audio.pause(); /*pauses old audio obj, if there is one*/
+  }
+  return audio = new Audio(url);
+}
+
+
+function toggleSong(song){  /*makes play button play/pause*/
+  if(audioState === true){
+    song.pause();
+    playBtn.setAttribute("src", "img/pause.png");
+    audioState = false;
+  }
+  else {
+    song.play();
+    playBtn.setAttribute("src", "img/playsolid.png");
+    audioState = true;
+  }
+}
+
+function playSong(song){ /* plays song and tethers progress bar to time*/
+  updateVolume();
+  song.play();
+  song.addEventListener("timeupdate", function(e){
+    progressBar.value = Math.floor(song.currentTime);
+  })
+  playBtn.setAttribute("src", "img/playsolid.png");
+  audioState = true;
+}
+
+function updateVolume(){
+  let volumeVal = volumeSlider.value / 100;
+  audio.volume = volumeVal;
+  console.log(volumeVal);
+}
+
+// =============== Search Function =====================
+
+
 
 function clearResults(){
   resultsCont.innerHTML = "";
-}
-
-function playSong(url){
-  player.setAttribute("src", url);
 }
 
 function searchMusic(){
@@ -38,21 +80,19 @@ function searchMusic(){
     .then( function (data) {
       return data.json();
     })
-    // .then( function (data) {
-    //   console.log(data)"
-    // })
     .then( function (data) {
 
       for(i=0;i<20;i++){
         let item = data.results;
         let tmpl =
           `<div class="resultItem">
-            <img class="albumArt" src="${item[i].artworkUrl100}" alt="Album Cover" onclick='playSong("${item[i].previewUrl}")'>
+            <img class="hoverPlay" src="img/playsolid.png" alt="">
+            <img class="albumArt" src="${item[i].artworkUrl100}" alt="Album Cover" onclick='playSong(createSong("${item[i].previewUrl}"))'>
             <a class="songTitle" href="${item[i].trackViewUrl}"><p>${item[i].trackName}</p></a>
             <a class="artistName" href="${item[i].artistViewUrl}"><p>${item[i].artistName}</p></a>
           </div>`;
 
-          resultsCont.innerHTML += tmpl;
+        resultsCont.innerHTML += tmpl;
       }
     })
     searchBar.value = "";
